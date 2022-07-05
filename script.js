a4063df42df5c701a30e6ea3ddecb26f60caa197
Luiz <lhvalentim@outlook.coom>
2022-07-05 09:21:57 -0300

    add ignored text

--- script.js
+++ script.js
@@ -8,27 +8,32 @@ var voice
 
 const dialog = [
 	{
-		text: 'Como assim você ainda não se inscreveu no Quiz de gestão por métricas ?',
-		balloon: 'right',
+		text: 'Hey [S G I T I](SGITI), estão bem ?',
+	},
+	{
+		text: 'Espero que sim, pois vim aqui especialmente\nconvocá-los para um desafio exclusivo!',
+		image: '6'
+	},
+	{
+		text: 'Na próxima Plenária vamos divulgar o pódio da [S G I T I](SGITI)\ncom os melhores colocados da rodada atual!',
+		image: '4'
+	},
+	{
+		text: 'Se você já jogou no período de homologação, teve uma colher de chá.\nSeus dados foram zerados para você poder jogar novamente.',
+		image: '5',
+		balloon: 'center'
+	},
+	{
+		text: 'E você que ainda nem entrou. Por que ?\nCorre que dá tempo de tirar o atraso!',
 		image: '1'
 	},
 	{
-		text: 'Não sabe do que eu tô falando?! Te explico.',
-		image: '3'
-	},
-	{
-		text: 'É um quiz para testar seus conhecimentos em gestão por métricas e ajudar você a entender os principais conceitos desse assunto.',
+		text: 'As primeiras pessoas que concluírem o Quiz com maior pontuação ficarão na frente.\nO ranking está súper acirrado, então corre!',
 		image: '4',
-		balloon: 'right'
 	},
 	{
-		text: 'E aí? Bora jogar?!',
-		image: '0'
-	},
-	{
-		text: 'Aaahhh! não esqueça de chamar todo o seu time. Ganhe pontos e mostre que você é fera!',
+		text: 'Vamos de turma!',
 		image: '2',
-		delay: 500
 	}
 ]
 
@@ -59,11 +64,19 @@ function setupImage() {
 }
 function setupText() {
 	if (dialog[currentDialog].balloon == 'right') {
-		caption.classList.add('balloon-right')
+		caption.classList.remove('center')
+		caption.classList.remove('left')
+		caption.classList.add('right')
+	} else if (dialog[currentDialog].balloon == 'center') {
+		caption.classList.add('center')
+		caption.classList.remove('left')
+		caption.classList.remove('right')
 	} else {
-		caption.classList.remove('balloon-right')
+		caption.classList.remove('center')
+		caption.classList.remove('right')
+		caption.classList.add('left')
 	}
-	caption.innerHTML = dialog[currentDialog].text
+	caption.innerHTML = dialog[currentDialog].text.replace(/\(|\)|\[.*\]/g, '')
 	setupVoice(dialog[currentDialog].text)
 }
 function setupVoice(text) {
@@ -79,7 +92,7 @@ function setupVoice(text) {
 		synth.voice = voice
 	}
 	synth.lang = voice?.lang ?? 'pt-BR'
-	synth.text = text
+	synth.text = text.replace(/\[|\]|\(.*\)/g, '').replace(/\n/g, ' ')
 	synth.pitch = 2
 	synth.rate = 1.5
 	speechSynthesis.speak(synth)
@@ -92,14 +105,14 @@ synth.onend = () => {
 window.onresize = () => {
 	resize()
 }
-document.onclick = () => {
+/* document.onclick = () => {
 	if (audioEnabled) return
 	synth.text = ''
 	synth.volume = 0
 	speechSynthesis.speak(synth)
 	audioEnabled = true
 	synth.volume = 1
-}
+} */
 document.onreadystatechange = () => {
 	if (document.readyState != 'complete') return
 	figure.classList.add('show')
